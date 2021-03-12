64ace385e53cca7650c59ef47bd6f3ebc13154a0
Bodeu97 <lukaszebo@gmail.com>
2021-03-12 17:29:28 +0100

    update

--- main.py
+++ main.py
@@ -127,5 +127,16 @@ from random import randint
 # print(seqProd(1, 2, 6))
 
 
+# # Zad7
+#
+# def seqProdUnl(* elementy):
+#     if(len(elementy) != 0):
+#         iloczyn = 1
+#         for x in range(len(elementy)):
+#             iloczyn*=elementy[x]
+#         return iloczyn
+#     else:
+#         return 0
+# print(seqProdUnl(1, 2, 3, 4, 5, 6))
 
 
